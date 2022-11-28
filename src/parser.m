4a7c03b862c90ecf2b025de073a733b1633751a3
Tanner Koza <jtk0018@auburn.edu>
2022-11-28 10:55:41 -0600

    Finish parser

--- src/parser.m
+++ src/parser.m
@@ -6,5 +6,16 @@ close all
 
 %% Parsing
 
-filePath = '../data/gps.csv';
-gpsTable = readtable(filePath);
+files = dir('../data/*.csv');
+
+for i=1:length(files)
+    
+    currentFilePath = strcat(files(i).folder, filesep, files(i).name);
+    [~, fileName, ~] = fileparts(currentFilePath);
+    data = readtable(currentFilePath);
+    structData = table2struct(data);
+    tempOutputStruct.(fileName) = structData;
+    
+end
+
+save('../data/fordav_sample_data','-struct','tempOutputStruct')
