085990fba64b3972ebbdcc67b94689e5c8135177
Tanner Koza <kozatanner@gmail.com>
2022-11-28 19:52:38 -0600

    Delete /tf grom list of topics to parse

--- src/bag_to_csv.py
+++ src/bag_to_csv.py
@@ -3,7 +3,7 @@ from tqdm import tqdm
 
 
 b = bagreader("/home/tannerkoza/edu/nav-project/data/2017-08-04-V3-Log3.bag")
-topics = ['/gps', '/gps_time', '/imu', '/pose_ground_truth', '/pose_localized', '/pose_raw', '/tf', '/velocity_raw']
+topics = ['/gps', '/gps_time', '/imu', '/pose_ground_truth', '/pose_localized', '/pose_raw', '/velocity_raw']
 
 csvfiles = []
 for t in tqdm(topics):
