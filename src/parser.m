caaa2c60bf5dc7ca6282a91dfeb9fb9d3e2500d2
Tanner Koza <jtk0018@auburn.edu>
2022-11-28 19:41:16 -0600

    Update parser to look inside all log directories

--- src/parser.m
+++ src/parser.m
@@ -6,16 +6,28 @@ close all
 
 %% Parsing
 
-files = dir('../data/*.csv');
+rootDir = fullfile(fileparts(which(mfilename)), "..");
+dataDir = fullfile(rootDir, "data");
+logs = dir(dataDir);
 
-for i=1:length(files)
-    
-    currentFilePath = strcat(files(i).folder, filesep, files(i).name);
-    [~, fileName, ~] = fileparts(currentFilePath);
-    data = readtable(currentFilePath);
-    structData = table2struct(data,"ToScalar",true);
-    tempOutputStruct.(fileName) = structData;
+for i=4:length(logs)
+
+    currentLogPath = fullfile(dataDir, logs(i).name, "*.csv");
+    files = dir(currentLogPath);
+
+    for j = 1:length(files)
+
+        currentFilePath = fullfile(files(j).folder, files(j).name);
+        [~, fileName, ~] = fileparts(currentFilePath);
+        data = readtable(currentFilePath);
+        structData = table2struct(data,"ToScalar",true);
+        tempOutputStruct.(fileName) = structData;
+
+    end
+
+    outputDir = fullfile(dataDir, logs(i).name, logs(i).name);
+    save(outputDir,'-struct','tempOutputStruct')
     
 end
 
-save('../data/fordav_sample_data','-struct','tempOutputStruct')
+
